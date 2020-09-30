ea7fc8f5bf0d131a6690dfd3c3cd93bd995c8617
Jonathan Quesada <jonnythan.quesada@gmail.com>
2020-09-30 15:37:11 -0700

    Have working html, js, and css imported elements & initalized state

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // import functions and grab DOM elements
+const throwPaper = document.querySelector('#paper');
+const throwScissors = document.querySelector('#scissors');
+const throwRock = document.querySelector('#paper');
+const playGame = document.getElementById('submit');
+const results = document.getElementById('results-span');
+const games = document.getElementById('games-span');
+const sets = document.getElementById('sets-span');
 
 // initialize state
+let setsWon = 0;
+let gamesWon = 0;
+let gamesPlayed = 0;
 
 // set event listeners to update state and DOM
